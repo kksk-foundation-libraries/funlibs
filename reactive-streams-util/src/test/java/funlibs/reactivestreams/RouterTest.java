ffefd1f884d48236ea6a34c00cbef550f6375498
kakusuke-maruno <kakusuke.maruno@gmail.com>
2020-12-17 00:28:30 +0900

    brush test code up.

--- reactive-streams-util/src/test/java/funlibs/reactivestreams/RouterTest.java
+++ reactive-streams-util/src/test/java/funlibs/reactivestreams/RouterTest.java
@@ -3,7 +3,6 @@ package funlibs.reactivestreams;
 import static org.junit.Assert.*;
 
 import java.util.LinkedList;
-import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.BiFunction;
 import java.util.function.Consumer;
@@ -12,6 +11,7 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import funlibs.concurrent.Blocker;
 import funlibs.logging.SimpleLogging;
 import reactor.core.publisher.Flux;
 
@@ -24,10 +24,10 @@ public class RouterTest {
 	public void test001() {
 		final AtomicReference<Consumer<Integer>> fluxSink1 = new AtomicReference<>();
 		final AtomicReference<Consumer<Integer>> fluxSink2 = new AtomicReference<>();
-		final CountDownLatch latch1_1 = new CountDownLatch(1);
-		final CountDownLatch latch1_2 = new CountDownLatch(1);
-		final CountDownLatch latch2_1 = new CountDownLatch(1);
-		final CountDownLatch latch2_2 = new CountDownLatch(1);
+		final Blocker blocker_1_1 = new Blocker();
+		final Blocker blocker_1_2 = new Blocker();
+		final Blocker blocker_2_1 = new Blocker();
+		final Blocker blocker_2_2 = new Blocker();
 		final LinkedList<String> result = new LinkedList<>();
 		Flux<Integer> flux1 = Flux.create(sink -> {
 			fluxSink1.set(t -> {
@@ -37,7 +37,7 @@ public class RouterTest {
 				}
 			});
 			sink.onCancel(() -> fluxSink1.set(null));
-			latch1_1.countDown();
+			blocker_1_1.unblock();
 		});
 		Flux<Integer> flux2 = Flux.create(sink -> {
 			fluxSink2.set(t -> {
@@ -47,36 +47,30 @@ public class RouterTest {
 				}
 			});
 			sink.onCancel(() -> fluxSink2.set(null));
-			latch1_2.countDown();
+			blocker_1_2.unblock();
 		});
 		BiFunction<String, String, Boolean> failureHandler = (signalType, emitResult) -> false;
 		Router<Integer> router1 = Router.direct(failureHandler);
 		flux1.subscribe(router1);
 		flux2.subscribe(router1);
 		router1.subscribe(t -> result.add("data..(1):" + t), t -> {
-		}, () -> latch2_1.countDown());
-		try {
-			latch1_1.await();
-			latch1_2.await();
-		} catch (InterruptedException e) {
-		}
+		}, () -> blocker_2_1.unblock());
+		blocker_1_1.blockSilent();
+		blocker_1_2.blockSilent();
 		int index = 0;
 		while (index < 10 && fluxSink1.get() != null) {
 			fluxSink1.get().accept(index);
 			index++;
 		}
 		router1.subscribe(t -> result.add("data..(2):" + t), t -> {
-		}, () -> latch2_2.countDown());
+		}, () -> blocker_2_2.unblock());
 		while (index < 20 && fluxSink2.get() != null) {
 			fluxSink2.get().accept(index);
 			index++;
 		}
 		router1.stop();
-		try {
-			latch2_1.await();
-			latch2_2.await();
-		} catch (InterruptedException e) {
-		}
+		blocker_2_1.blockSilent();
+		blocker_2_2.blockSilent();
 		LOG.info("result:{}", result);
 		assertTrue("processed will be 30", result.size() == 30);
 		LOG.info("completed.");
