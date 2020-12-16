dec4d2e838362396dc296cbd9d323e3edf8a3809
kakusuke-maruno <kakusuke.maruno@gmail.com>
2020-12-17 01:16:19 +0900

    add assertion codes.

--- concurrent-util/src/test/java/funlibs/concurrent/BlockerTest.java
+++ concurrent-util/src/test/java/funlibs/concurrent/BlockerTest.java
@@ -1,5 +1,8 @@
 package funlibs.concurrent;
 
+import static org.junit.Assert.assertTrue;
+
+import java.util.LinkedList;
 import java.util.concurrent.CompletableFuture;
 
 import org.junit.Test;
@@ -17,35 +20,53 @@ public class BlockerTest {
 	public void test001() {
 		LOG.debug("test001");
 		final Blocker blocker = new Blocker();
+		final LinkedList<String> result = new LinkedList<>();
 		CompletableFuture<Void> future = CompletableFuture.runAsync(() -> {
 			ConcurrentUtil.sleepSlient(10);
+			result.add("before unblock");
 			LOG.debug("before unblock");
 			ConcurrentUtil.sleepSlient(100);
 			blocker.unblock();
+			result.add("after unblock");
 			LOG.debug("after unblock");
 		});
+		result.add("before blockSilent");
 		LOG.debug("before blockSilent");
 		ConcurrentUtil.sleepSlient(30);
 		blocker.blockSilent();
+		result.add("after blockSilent");
 		LOG.debug("after blockSilent");
 		ConcurrentUtil.getSilent(future);
+		assertTrue("will be 'before blockSilent'", "before blockSilent".equals(result.get(0)));
+		assertTrue("will be 'before unblock'", "before unblock".equals(result.get(1)));
+		assertTrue("will be 'after unblock'", "after unblock".equals(result.get(2)));
+		assertTrue("will be 'after blockSilent'", "after blockSilent".equals(result.get(3)));
 	}
 
 	@Test
 	public void test002() {
 		LOG.debug("test002");
 		final Blocker blocker = new Blocker();
+		final LinkedList<String> result = new LinkedList<>();
 		CompletableFuture<Void> future = CompletableFuture.runAsync(() -> {
 			ConcurrentUtil.sleepSlient(10);
+			result.add("before unblock");
 			LOG.debug("before unblock");
 			ConcurrentUtil.sleepSlient(100);
 			blocker.unblock();
+			result.add("after unblock");
 			LOG.debug("after unblock");
 		});
+		result.add("before blockSilent");
 		LOG.debug("before blockSilent");
 		ConcurrentUtil.sleepSlient(30);
 		blocker.blockSilent(10);
+		result.add("after blockSilent");
 		LOG.debug("after blockSilent");
 		ConcurrentUtil.getSilent(future);
+		assertTrue("will be 'before blockSilent'", "before blockSilent".equals(result.get(0)));
+		assertTrue("will be 'before unblock'", "before unblock".equals(result.get(1)));
+		assertTrue("will be 'after blockSilent'", "after blockSilent".equals(result.get(2)));
+		assertTrue("will be 'after unblock'", "after unblock".equals(result.get(3)));
 	}
 }
